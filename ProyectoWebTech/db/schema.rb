dc1bec0d7ed69baf475983328047d34e03cfdb97
PedroNaretto <ppnaretto@miuandes.cl>
2020-06-22 17:49:45 -0400

    devise user

--- ProyectoWebTech/db/schema.rb
+++ ProyectoWebTech/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_230949) do
+ActiveRecord::Schema.define(version: 2020_06_22_214556) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -242,6 +242,13 @@ ActiveRecord::Schema.define(version: 2020_06_04_230949) do
     t.datetime "last_acces"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "last_sign_in_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
