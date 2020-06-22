17e520cc19016d1b91801e660f7f7cd535590662
PedroNaretto <ppnaretto@miuandes.cl>
2020-06-22 18:02:04 -0400

    Signs

--- ProyectoWebTech/app/controllers/application_controller.rb
+++ ProyectoWebTech/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  def user_id
-    #@user_id =
+  before_action :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:name, :email, :password, :password_confirmation, :location, :profile_photo)
+    end
   end
 end
