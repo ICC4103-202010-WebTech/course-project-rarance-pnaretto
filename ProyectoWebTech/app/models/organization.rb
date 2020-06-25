1d132de490291e787251f5bbb2f2ae73c0a3e199
PedroNaretto <ppnaretto@miuandes.cl>
2020-06-24 22:54:10 -0400

    Algunos detalles

--- ProyectoWebTech/app/models/organization.rb
+++ ProyectoWebTech/app/models/organization.rb
@@ -1,10 +1,10 @@
 class Organization < ApplicationRecord
 
+  has_one_attached :organization_banner_photo, dependent: :delete_all
+
   belongs_to :user, optional: true
   has_many :organization_members
   has_many :organization_files
   has_many :organization_reports
   has_many :organization_events
-
-  has_one_attached :organization_banner_photo, dependent: :delete_all
 end
